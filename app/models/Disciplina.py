29f9c139068d6ca960f1b51cfc0b9f72949b5687
vanagila <vanagilakedna@gmail.com>
2026-05-13 20:50:17 -0300

    carga horária adicionada a disciplinas

--- app/models/Disciplina.py
+++ app/models/Disciplina.py
@@ -8,6 +8,7 @@ class Disciplina(Base):
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String(100), nullable=False)
     codigo = Column(String(20), unique=True, nullable=False)
-    descricao = Column(String(500))
+    descricao = Column(String(500), nullable=True)
+    carga_horaria = Column(Integer, nullable=False, default=60)
 
     turmas = relationship("Turma", back_populates="disciplina")
